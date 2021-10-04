43fa4bb8d44a3503ef4300995a34241a05e62902
elBibi <my.ajt.knight@gmail.com>
2021-10-04 10:10:52 -0400

    Hello world with SpringBoot

---
+++ src/main/java/com/example/helloworldrestfulservice/HelloWorldRestfulServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.helloworldrestfulservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldRestfulServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorldRestfulServiceApplication.class, args);
+    }
+
+}
